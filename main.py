d102fb717e3a8ba959db54cc07cee8b7f4af3ed4
nicolacavallini <nicola.cavallini@unipv.it>
2016-06-28 14:54:02 +0200

    grafico

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import numpy as np
+import matplotlib.pyplot as plt
 print 'Hello World'
 m=np.zeros((10,20))
 print m
-x=np.linspace(0,1,20)
+x=np.linspace(0,2*np.pi,20)
 print x
 y=np.linspace(0,1,30)
 print y
+y=np.sin(x)
+
+plt.plot(x,y,'-o')
+plt.show()
